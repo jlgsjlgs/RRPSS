fe22ecdc510d10f686bdd3d17550fe575d9a1735
brandon <93000183+brandonlokemk@users.noreply.github.com>
2021-10-29 16:14:22 +0800

    Update RRSPSS.java

--- RRSPSS.java
+++ RRSPSS.java
@@ -1,81 +1,81 @@
 public class RRSPSS {
 
-	public void addItem() {
+	void addItem() {
 		// TODO - implement RRSPSS.addItem
 		throw new UnsupportedOperationException();
 	}
 
-	public void removeItem() {
+	void removeItem() {
 		// TODO - implement RRSPSS.removeItem
 		throw new UnsupportedOperationException();
 	}
 
-	public void updateItem() {
+	void updateItem() {
 		// TODO - implement RRSPSS.updateItem
 		throw new UnsupportedOperationException();
 	}
 
-	public void addPromotional() {
+	void addPromotional() {
 		// TODO - implement RRSPSS.addPromotional
 		throw new UnsupportedOperationException();
 	}
 
-	public void removePromotional() {
+	void removePromotional() {
 		// TODO - implement RRSPSS.removePromotional
 		throw new UnsupportedOperationException();
 	}
 
-	public void updatePromotional() {
+	void updatePromotional() {
 		// TODO - implement RRSPSS.updatePromotional
 		throw new UnsupportedOperationException();
 	}
 
-	public void createOrder() {
+	void createOrder() {
 		// TODO - implement RRSPSS.createOrder
 		throw new UnsupportedOperationException();
 	}
 
-	public void viewOrder() {
+	void viewOrder() {
 		// TODO - implement RRSPSS.viewOrder
 		throw new UnsupportedOperationException();
 	}
 
-	public void addItemToOrder() {
+	void addItemToOrder() {
 		// TODO - implement RRSPSS.addItemToOrder
 		throw new UnsupportedOperationException();
 	}
 
-	public void removeItemFromOrder() {
+	void removeItemFromOrder() {
 		// TODO - implement RRSPSS.removeItemFromOrder
 		throw new UnsupportedOperationException();
 	}
 
-	public void createReservation() {
+	void createReservation() {
 		// TODO - implement RRSPSS.createReservation
 		throw new UnsupportedOperationException();
 	}
 
-	public void checkReservationa() {
+	void checkReservationa() {
 		// TODO - implement RRSPSS.checkReservationa
 		throw new UnsupportedOperationException();
 	}
 
-	public void removeReservation() {
+	void removeReservation() {
 		// TODO - implement RRSPSS.removeReservation
 		throw new UnsupportedOperationException();
 	}
 
-	public void checkTableAvailability() {
+	void checkTableAvailability() {
 		// TODO - implement RRSPSS.checkTableAvailability
 		throw new UnsupportedOperationException();
 	}
 
-	public void printOrderInvoice() {
+	void printOrderInvoice() {
 		// TODO - implement RRSPSS.printOrderInvoice
 		throw new UnsupportedOperationException();
 	}
 
-	public void printSaleRevenue() {
+	void printSaleRevenue() {
 		// TODO - implement RRSPSS.printSaleRevenue
 		throw new UnsupportedOperationException();
 	}
