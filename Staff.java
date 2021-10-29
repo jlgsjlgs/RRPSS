d2259e0cb2fbd8c55417b7a6bf775cddff6a1670
sadcsundergrad <58691416+jlgsjlgs@users.noreply.github.com>
2021-10-29 14:27:27 +0800

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,9 +1,40 @@
+import java.util.Scanner;
+
 public class Staff {
 
 	private String name;
 	private String gender;
 	private int employeeID;
 	private StaffRole jobTitle;
+        
+        public Staff(String name, String gender, int ID){
+            this.name = name;
+            this.gender = gender;
+            this.employeeID = ID;
+        }
+
+        
+	public void setJobTitle() {
+            Scanner sc = new Scanner(System.in);
+            
+            System.out.println("Job titles:");
+            System.out.println("1. Server");
+            System.out.println("2. Manager");
+            System.out.println("3. Cashier");
+            int userInput = Integer.valueOf(sc.nextLine());
+            
+            switch(userInput){
+                case 1:
+                    this.jobTitle = StaffRole.SERVER;
+                    break;
+                case 2:
+                    this.jobTitle = StaffRole.MANAGER;
+                    break;
+                case 3:
+                    this.jobTitle = StaffRole.CASHIER;
+                    break;
+            }
+        }
 
 	public String getName() {
 		return this.name;
@@ -19,14 +50,6 @@ public class Staff {
 
 	public StaffRole getJobTitle() {
 		return this.jobTitle;
-	}
-
-	/**
-	 * 
-	 * @param jobTitle
-	 */
-	public void setJobTitle(StaffRole jobTitle) {
-		this.jobTitle = jobTitle;
 	}
 
 }
