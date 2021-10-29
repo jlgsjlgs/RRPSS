2252212da73a83522ad7a9b9a380b749b5081679
brandon <93000183+brandonlokemk@users.noreply.github.com>
2021-10-29 16:17:58 +0800

    Update StaffRoster.java

--- StaffRoster.java
+++ StaffRoster.java
@@ -1,15 +1,15 @@
 import java.util.*;
 
-public class StaffRoster {
+class StaffRoster {
 
 	ArrayList<Staff> availStaff;
 	
-	public void addStaff(String name, String gender, int ID) {
+	void addStaff(String name, String gender, int ID) {
 		Staff newStaff = new Staff(name, gender, ID);
 		availStaff.add(newStaff);
 	}
 	
-	public void removeStaff(int ID) {
+	void removeStaff(int ID) {
 		for (int i = 0; i < availStaff.size(); i++) {
 			if (availStaff.get(i).getemployeeID() == ID) availStaff.remove(i);
 		}
