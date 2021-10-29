457d0525dfd0f8f4a08b665d7a7838d0bbec0de5
brandon <93000183+brandonlokemk@users.noreply.github.com>
2021-10-29 19:03:22 +0800

    Add files via upload

---
+++ AutoStaffRosterTest.java
@@ -0,0 +1,12 @@
+package CZ2002_OOP_Proj;
+
+public class AutoStaffRosterTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaffRoster staffTeam = new StaffRoster();
+		staffTeam.bootup();
+		staffTeam.showStaff();
+	}
+
+}
