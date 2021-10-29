c63845b853e4dd7f86a26d955f67e351f297a1a5
janessir <70764585+janessir@users.noreply.github.com>
2021-10-29 19:54:40 +0800

    Update RRSPSS.java

--- RRSPSS.java
+++ RRSPSS.java
@@ -1,105 +1,151 @@
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeParseException;
-import java.util.Scanner;
+public class RRSPSS {
 
-class RRSPSS {
+	//bootstart
+	//create seatingmanagement sm
+	//create ReservationList rl
 
-	void addItem() {
+	//editing menu
+
+	public void addItem() {
 		// TODO - implement RRSPSS.addItem
 		throw new UnsupportedOperationException();
 	}
 
-	void removeItem() {
+	public void removeItem() {
 		// TODO - implement RRSPSS.removeItem
 		throw new UnsupportedOperationException();
 	}
 
-	void updateItem() {
+	public void updateItem() {
 		// TODO - implement RRSPSS.updateItem
 		throw new UnsupportedOperationException();
 	}
 
-	void addPromotional() {
+	//promotion
+	public void addPromotional() {
 		// TODO - implement RRSPSS.addPromotional
 		throw new UnsupportedOperationException();
 	}
 
-	void removePromotional() {
+	public void removePromotional() {
 		// TODO - implement RRSPSS.removePromotional
 		throw new UnsupportedOperationException();
 	}
 
-	void updatePromotional() {
+	public void updatePromotional() {
 		// TODO - implement RRSPSS.updatePromotional
 		throw new UnsupportedOperationException();
 	}
 
-	void createOrder() {
-		// TODO - implement RRSPSS.createOrder
-		throw new UnsupportedOperationException();
+	//reservation
+
+	void createReservation(String name, int pax) {
+
+		Scanner scan = new Scanner(System.in);
+
+		//check if table available
+		// if yes then actually creates reservation , long phoneNum, Boolean mem
+		if(checkTableAvailability(pax)){
+
+			//get additional info
+			System.out.println("can i have ur number");
+				long phoneNum = scan.nextLong();
+			System.out.println("bro u member?");
+				String memS = scan.next(); //y/n
+				Boolean mem = memS.charAt(0).toLowerCase() == 'y' ;
+
+			System.out.println("Enter date in YYYY-MM-DD format");
+			LocalDate date;
+			LocalTime time;
+			while(true){
+				try {
+					date = LocalDate.parse(scan.next());
+					break;
+				}catch (DateTimeParseException e){
+					System.out.println("Invalid date");
+				}
+			}
+			while(true){
+				try {
+					time = LocalTime.parse(scan.next());
+					break;
+				}catch (DateTimeParseException e){
+					System.out.println("Invalid date");
+				}
+			}
+
+			//create reservation
+			rl.addReservation(String name, int pax, long phoneNum, LocalDate date, LocalTime time, Boolean mem); 
+		}
+
+		// else say sorry, no reservation obj created
+		else{
+			System.out.println("No tables with your current pax available atm, try reserving again tomorrow or put ut child at home");
+		}
+		
+		
 	}
 
-	void viewOrder() {
+	public Boolean checkReservation(long rID) {
+		if(rl.getReservation(rID) != null)
+			return true;
+		else return false;
+	}
+
+	public void removeReservation() {
+		if(rl.getReservation(rID) != null)
+			rl.removeReservation(rID);
+	}
+
+	//check table
+	public Boolean checkTableAvailability(int pax) {
+		if (sm.getAvailTable(pax) <0 )
+			return false;
+		else return true;
+	}
+
+
+	//order
+
+	public void createOrder() {
+		// check if there's reservation, ie has valid rID, yes-> create order, else deny
+
+		//ask for rID
+		Scanner scan = new Scanner(System.in);
+		System.out.println("What's ur rID?");
+		long rID = scan.nextLong();
+
+		if(checkReservation(rID)){
+			Order(rID, staff, sm.); // to be finished
+		}
+	}
+
+	public void viewOrder() {
 		// TODO - implement RRSPSS.viewOrder
 		throw new UnsupportedOperationException();
 	}
 
-	void addItemToOrder() {
+	public void addItemToOrder() {
 		// TODO - implement RRSPSS.addItemToOrder
 		throw new UnsupportedOperationException();
 	}
 
-	void removeItemFromOrder() {
+	public void removeItemFromOrder() {
 		// TODO - implement RRSPSS.removeItemFromOrder
 		throw new UnsupportedOperationException();
 	}
 
-	void createReservation() {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter date in YYYY-MM-DD format");
-		LocalDate date;
-		LocalTime time;
-		while(true){
-			try {
-				date = LocalDate.parse(scan.next());
-				break;
-			}catch (DateTimeParseException e){
-				System.out.println("Invalid date");
-			}
-		}
-		while(true){
-			try {
-				time = LocalTime.parse(scan.next());
-				break;
-			}catch (DateTimeParseException e){
-				System.out.println("Invalid date");
-			}
-		}
-		//create reservation
-	}
+	
+	//check out
 
-	void checkReservationa() {
-		// TODO - implement RRSPSS.checkReservationa
-		throw new UnsupportedOperationException();
-	}
-
-	void removeReservation() {
-		// TODO - implement RRSPSS.removeReservation
-		throw new UnsupportedOperationException();
-	}
-
-	void checkTableAvailability() {
-		// TODO - implement RRSPSS.checkTableAvailability
-		throw new UnsupportedOperationException();
-	}
-
-	void printOrderInvoice() {
+	public void printOrderInvoice() {
 		// TODO - implement RRSPSS.printOrderInvoice
 		throw new UnsupportedOperationException();
 	}
 
-	void printSaleRevenue() {
+	//wrapping up
+
+	public void printSaleRevenue() {
 		// TODO - implement RRSPSS.printSaleRevenue
 		throw new UnsupportedOperationException();
 	}
