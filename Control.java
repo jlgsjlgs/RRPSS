a491b9ec055dc352dbb8ad18b67a14b8e16a7070
janessir <70764585+janessir@users.noreply.github.com>
2021-10-29 15:53:24 +0800

    Update Control.java

--- Control.java
+++ Control.java
@@ -3,17 +3,19 @@ public class Control{
 		SeatingManagement sm = new SeatingManagement();
 		int tID;
 
-		for(int i = 0; i < 20; i++){
-			tID = sm.getAvailTable(2);
-			sm.reserveATable(tID);
-			sm.assignTable(tID);
-			System.out.println(sm.getTable(tID).getStatus());
-			System.out.println(sm.getTable(tID).getOrder());
-			System.out.println(sm.getTable(tID).getTableID());
-			System.out.println(sm.getTable(tID).getNumOfSeats());
-			System.out.println();
-		}
-		
 
+		for(int i = 0; i < 20; i++){
+			tID = sm.getAvailTable(4);
+			if (tID >=1 ){
+				sm.reserveATable(tID);
+				System.out.println(sm.getTable(tID).getStatus());
+				System.out.println(sm.getTable(tID).getOrder());
+				System.out.println(sm.getTable(tID).getTableID());
+				System.out.println(sm.getTable(tID).getNumOfSeats());
+				System.out.println();
+		}
+
+			
+		}
 	}
 }
