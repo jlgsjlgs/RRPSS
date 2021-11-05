2ac4dac24c96d9890f033fc0367a82fe135ef4da
sadcsundergrad <58691416+jlgsjlgs@users.noreply.github.com>
2021-11-05 22:40:12 +0800

    Add files via upload

--- Drink.java
+++ Drink.java
@@ -1,2 +1,16 @@
-class Drink extends MenuItem {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jonat
+ */
+public class Drink extends MenuItem{
+    
+    public Drink(String name, String description, double price){
+        super(name, description, price);
+    }
 }
--- MainCourse.java
+++ MainCourse.java
@@ -1,2 +1,17 @@
-class MainCourse extends MenuItem {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jonat
+ */
+public class MainCourse extends MenuItem{
+    
+    public MainCourse(String name, String description, double price){
+        super(name, description, price);
+    }
+    
 }
