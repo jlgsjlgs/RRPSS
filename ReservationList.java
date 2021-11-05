cdcd4c36b9276f23d521dda75e37f6c3af1d4fde
janessir <70764585+janessir@users.noreply.github.com>
2021-11-05 15:35:26 +0800

    Update ReservationList.java

--- ReservationList.java
+++ ReservationList.java
@@ -2,19 +2,23 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
-public class ReservationList {
-
-	StaffRoster staff = new StaffRoster();
-	private final ArrayList<Reservation> resList = new ArrayList <>();
-	private static int nextrID = 1;
+class ReservationList {
+	
+	private ArrayList<Reservation> resList;
+	private static final int NEXT_RID;
+	
+	ReservationList(){
+		resList = new ArrayList <Reservation>();
+		NEXT_RID = 1;
+	}
 
 	ArrayList<Reservation> getResList() {
 		return resList;
 	}
 
 	void addReservation(String name, int pax, long phoneNum, LocalDate resDate, LocalTime resTime, Boolean mem) {
-		resList.add(new Reservation(name, pax, phoneNum, resDate, resTime, mem, nextrID));
-		nextrID++;
+		resList.add(new Reservation(name, pax, phoneNum, resDate, resTime, mem, NEXT_RID));
+		NEXT_RID++;
 	}
 
 	void removeReservation(long rID) {
