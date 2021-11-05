8f6a22eae8573592dd6164700a0a07843212c877
sadcsundergrad <58691416+jlgsjlgs@users.noreply.github.com>
2021-11-05 23:08:57 +0800

    Add files via upload

--- Dessert.java
+++ Dessert.java
@@ -1,16 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author jonat
- */
-public class Dessert extends MenuItem{
+class Dessert extends MenuItem{
     
-    public Dessert(String name, String description, double price){
+    Dessert(String name, String description, double price){
         super(name, description, price);
     }
 }
--- MainCourse.java
+++ MainCourse.java
@@ -1,16 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author jonat
- */
-public class MainCourse extends MenuItem{
+class MainCourse extends MenuItem{
     
-    public MainCourse(String name, String description, double price){
+    MainCourse(String name, String description, double price){
         super(name, description, price);
     }
     
