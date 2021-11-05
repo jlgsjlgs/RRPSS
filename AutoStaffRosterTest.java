884aeae6fa6705c353c1d6727bf2eaec7345ffcb
sadcsundergrad <58691416+jlgsjlgs@users.noreply.github.com>
2021-11-05 23:02:48 +0800

    Delete AutoStaffRosterTest.java

--- AutoStaffRosterTest.java
+++
@@ -1,9 +0,0 @@
-public class AutoStaffRosterTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		StaffRoster staffTeam = new StaffRoster();
-		staffTeam.bootup();
-		staffTeam.showStaff();
-	}
-}
